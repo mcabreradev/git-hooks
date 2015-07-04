147a6a28f9658af1eda5fdf61ed48d3953a32bbe
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2015-07-04 12:19:52 -0300

    test 6

--- sh.sh
+++ sh.sh
@@ -37,7 +37,7 @@ FORBIDDEN_BRANCHES="^(master)$"
 PROTECTED_BRANCHES="^(dev|release-*|patch-*)$"
 BRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
-if [[ "$BRANCH" == "$FORBIDDEN_BRANCHES" ]]; then
+if [[ "$BRANCH" =~ "$FORBIDDEN_BRANCHES" ]]; then
   echo "Prevented force-push to $BRANCH. This is a very dangerous command."
   echo "If you really want to do this, use --no-verify to bypass this pre-push hook."
   exit 1
